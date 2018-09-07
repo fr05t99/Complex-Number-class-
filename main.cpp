a3f8d5decf0ba3061d5e5b20624c6101df0f4007
fr05t99 <dwijrastogi100@gmail.com>
2018-09-07 11:58:51 +0530

    Checked addition and subtraction

--- main.cpp
+++ main.cpp
@@ -3,8 +3,11 @@
 
 int main()
 {
-    Complex c;
-    //std::cin >> c;
-    //std::cout << c;
+    Complex c1(1, 9);
+    Complex c2(1, -9);
+    //std::cin >> c1;
+    //std::cout << c1;
+    Complex c3 = c1 - c2;
+    std::cout << c3;
     return 0;
 }
