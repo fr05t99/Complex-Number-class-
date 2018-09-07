aaa463a8e6d436bbb8fa685329e858b83dd49d50
fr05t99 <dwijrastogi100@gmail.com>
2018-09-07 11:57:15 +0530

    Added Addition and Subtraction

--- Complex.cpp
+++ Complex.cpp
@@ -1,7 +1,5 @@
 #include "Complex.h"
 #include<iostream>
-#include<ostream>
-#include<istream>
 
 Complex::Complex()
 {
@@ -31,13 +29,36 @@ void Complex::setImaginary(int i)
     imaginary = i;
 }
 
-std::ostream & operator<<(std::ostream & os, Complex &c)
+Complex Complex::operator + (Complex &c)
 {
-    os << std::endl << c.real << " + " << c.imaginary << "i";
+    Complex temp;
+    temp.real = real + c.real;
+    temp.imaginary = imaginary + c.imaginary;
+    return temp;
+}
+
+Complex Complex::operator - (Complex &c)
+{
+    Complex temp;
+    temp.real = real - c.real;
+    temp.imaginary = imaginary - c.imaginary;
+    return temp;
+}
+
+std::ostream & operator<<(std::ostream &os, Complex &c)
+{
+    if (c.imaginary >= 0)
+    {
+        os << std::endl << c.real << " + " << c.imaginary << "i";
+    }
+    else
+    {
+        os << std::endl << c.real << " - " << -(c.imaginary) << "i";
+    }
     return os;
 }
 
-std::istream & operator>>(std::istream & is, Complex &c)
+std::istream & operator>>(std::istream &is, Complex &c)
 {
     int r , i;
     std::cout << "\nEnter real part : ";
