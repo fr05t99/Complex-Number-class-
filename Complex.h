c0a5def895b3e46744a5185e4cf7231076a8f91a
fr05t99 <dwijrastogi100@gmail.com>
2018-09-09 03:01:34 +0530

    Added division and reciprocal. Changed integer to float.

--- Complex.h
+++ Complex.h
@@ -5,20 +5,38 @@
 class Complex
 {
     public:
+        //Constructors
         Complex();
-        Complex(int);
-        Complex(int, int);
-        void setImaginary(int);
-        void setReal(int);
+        Complex(float);
+        Complex(float, float);
+
+        //Getters and Setters
+        void setImaginary(float);
+        void setReal(float);
+        float getReal();
+        float getImaginary();
+
+        //Operator Overloaded Functions
         Complex operator + (Complex &c);
         Complex operator - (Complex &c);
+        Complex operator * (Complex &c);
+        Complex operator / (Complex &c);
+        void operator ! ();
+
+        //Friend Functions
+        friend Complex operator * (float a, Complex &c);
         friend std::ostream & operator<<(std::ostream &os, Complex &c);
         friend std::istream & operator>>(std::istream &is, Complex &c);
+
+        //Member Functions
+        Complex reciprocal();
+
+
     protected:
 
     private:
-        int real;
-        int imaginary;
+        float real;
+        float imaginary;
 };
 
 #endif // COMPLEX_H
