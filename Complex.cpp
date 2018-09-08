54a0cc21f162f431954d6020a3051eea366210ba
fr05t99 <dwijrastogi100@gmail.com>
2018-09-09 03:00:25 +0530

    Added Division and Reciprocal. Changed integer to float

--- Complex.cpp
+++ Complex.cpp
@@ -7,26 +7,46 @@ Complex::Complex()
     imaginary = 0;
 }
 
-Complex::Complex(int r)
+Complex::Complex(float r)
 {
     real = r;
     imaginary = 0;
 }
 
-Complex::Complex(int r, int i)
+Complex::Complex(float r, float i)
 {
     real = r;
     imaginary = i;
 }
 
-void Complex::setReal(int r)
+void Complex::setReal(float r)
 {
     real = r;
 }
 
-void Complex::setImaginary(int i)
+void Complex::setImaginary(float i)
 {
     imaginary = i;
+}
+
+float Complex::getReal()
+{
+    return real;
+}
+
+float Complex::getImaginary()
+{
+    return imaginary;
+}
+
+Complex Complex::reciprocal()
+{
+    Complex temp;
+    float denominator = real * real + imaginary * imaginary;
+    temp.real = real / denominator;
+    temp.imaginary = imaginary / denominator;
+    temp.imaginary = -temp.imaginary;
+    return temp;
 }
 
 Complex Complex::operator + (Complex &c)
@@ -45,6 +65,40 @@ Complex Complex::operator - (Complex &c)
     return temp;
 }
 
+Complex Complex::operator * (Complex &c)
+{
+    Complex temp;
+    temp.real = (real * c.real) - (imaginary * c.imaginary);
+    temp.imaginary = (imaginary * c.real) + (real * c.imaginary);
+    return temp;
+}
+
+Complex Complex::operator / (Complex &c)
+{
+    Complex temp;
+    float denominator = c.real * c.real + c.imaginary * c.imaginary;
+    temp.real = (real * c.real + imaginary * c.imaginary) / denominator;
+    temp.imaginary = (imaginary * c.real - real * c.imaginary) / denominator;
+    return temp;
+}
+
+
+Complex operator * (float a, Complex &c)
+{
+    float r, i;
+    Complex temp;
+    r = c.getReal() * a;
+    i = c.getImaginary() * a;
+    temp.setImaginary(i);
+    temp.setReal(r);
+    return temp;
+}
+
+void Complex::operator ! ()
+{
+    imaginary = -imaginary;
+}
+
 std::ostream & operator<<(std::ostream &os, Complex &c)
 {
     if (c.imaginary >= 0)
@@ -60,7 +114,7 @@ std::ostream & operator<<(std::ostream &os, Complex &c)
 
 std::istream & operator>>(std::istream &is, Complex &c)
 {
-    int r , i;
+    float r , i;
     std::cout << "\nEnter real part : ";
     std::cin >> r;
     c.setReal(r);
